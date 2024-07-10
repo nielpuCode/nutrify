409c556437eb6b0ef640be3e56c79b8518fc4c4a
Daniel Mw <danielmw101@gmail.com>
2024-07-10 12:24:25 +0700

    fixing vercel

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 // src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "@/app/lib/auth";
+import { authOptions } from "../../../lib/auth.js";
 
 const handler = NextAuth(authOptions);
 
